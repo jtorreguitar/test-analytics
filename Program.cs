de0107af380e025e975dac829847136a6f7ef581
jtorreguitar <jtorreguitar@itba.edu.ar>
2019-10-23 13:38:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+// holu
+
 namespace test_analytics
 {
     public class Program
